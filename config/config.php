d1f010ca460d016abe543e54010eceb026a770f1
nafisroni <bawangm74@gmail.com>
2022-05-17 11:10:34 +0700

    Masukan Roni

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+$name_top ="#PANDAWA | Status";
+$name ="#PANDAWA";
+$ip = "g1.pandawarp.my.id";
+$port = "30120";
+$discord_url = "https://discord.gg/UR9xHGRwcN";
+#$vote_url = "";
+$max_slots = "64";
+?>
